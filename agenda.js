0fdc27d86bbef6c0cb14bf66c1d1f4c50142658b
BenjaminConcha <benjaminconcha1415@gmail.com>
2024-07-01 18:25:01 -0300

    Git no ignora carpeta Dist

--- agenda.js
+++ agenda.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const path = require('path')
+// const path = require('path')
 
 // const fs = require('fs')
 
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
 
 app.use(express.json());
 
